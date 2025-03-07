21c87af887524c9d92d35956ae1fca0988281fee
Ian D M Eufrazino <iandigori12@gmail.com>
2025-03-07 18:29:45 -0300

    Deploy de backend no Render

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -88,7 +88,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chatgrupo-backend.onrender.com")
     websocket.onmessage = processMessage
 }
 
